3aec996a34bfb6524de5a4507599a97f01457c4d
Daryl Noyce <djn-hgs@Daryls-MacBook-Air.local>
2019-02-26 16:48:26 +0000

    Initial Commit

---
+++ Noughts and Crosses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Noughts and Crosses
+//
+//  Created by Daryl Noyce (School) on 26/02/2019.
+//  Copyright © 2019 Daryl Noyce (School). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Noughts and CrossesTests/Noughts_and_CrossesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Noughts_and_CrossesTests.swift
+//  Noughts and CrossesTests
+//
+//  Created by Daryl Noyce (School) on 26/02/2019.
+//  Copyright © 2019 Daryl Noyce (School). All rights reserved.
+//
+
+import XCTest
+@testable import Noughts_and_Crosses
+
+class Noughts_and_CrossesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
